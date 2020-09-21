cc4cb467444b1763c983fb2341e763e65cbab79e
Stefan <stefan.jahn332@gmail.com>
2020-09-21 13:33:26 +0200

    initial commit

---
+++ App/src/main.c
@@ -0,0 +1,16 @@
+
+#include "stm32f10x.h"
+#include "arm_math.h"
+#include "arm_const_structs.h"
+#include "stdint.h"
+
+
+int main(void) {
+    uint32_t i = 0;
+
+    while(1){
+        i++;
+    }
+
+
+}
